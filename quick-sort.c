45b7a1f41bba7047d46e8f836d26864adadd1086
GondowTakashi <dokyogames@dokyougemusu-no-MacBook-Pro-2.local>
2014-08-26 16:28:18 +0900

    バブルソート追加

--- quick-sort.c
+++ quick-sort.c
@@ -1,6 +1,8 @@
+//クイックソートバブルソートを含む
 #include  <stdio.h>
 
 void QSort(int x[10], int left, int right);
+void BubSort(int x[10], int n);
 void Swap(int x[10], int i, int j);
 void ShowData(int x[10]);
 int main(void);
@@ -37,6 +39,23 @@ void QSort(int x[10], int left, int right)
     if (j + 1 <  right)            /* 基準値の右に 2 以上要素があれば */
         QSort(x, j + 1, right);    /* 右の配列を Q ソートする */
 }
+  /* バブルソートを行う */
+void BubSort(int x[10], int n)
+{
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = n - 1; j > i; j--) {
+            if (x[j - 1] > x[j]) {  /* 前の要素の方が大きかったら */
+                temp = x[j];        /* 交換する */
+                x[j] = x[j - 1];
+                x[j - 1]= temp;
+            }
+        }   
+        /* ソートの途中経過を表示 */
+        ShowData(x);
+    }
+}
 
   /* 配列の要素を交換する */
 void Swap(int x[10], int i, int j)
@@ -61,19 +80,29 @@ void ShowData(int x[10])
 
 int main(void)
 {      /* ソートする配列 */
-	int x[10];
-	for(int i=0;i<10;i++)
-		scanf("%d",&x[i]);
+        int x[10],y[10];
+        for(int i=0;i<10;i++){
+                scanf("%d",&x[i]);
+                y[i]=x[i];
+            }
 
+    //クイックソート部
     printf("ソート前:\n");
     ShowData(x);
-
     printf("ソート中:\n");
-    QSort(x, 0,10 - 1);
-
+    BubSort(x,10);
     printf("ソート後:\n");
     ShowData(x);
 
-	return 0;
-}
+    //バブルソート部
+    printf("ソート前:\n");
+    ShowData(y);
+    printf("ソート中:\n");
+    QSort(y, 0,10 - 1);
+    printf("ソート後:\n");
+    ShowData(y);
 
+
+
+        return 0;
+}
